d4092249eccaad5e9b0ada2f4a8425ab2e395542
Owhondah Okechukwu Samuel <60843008+Psami-wondah@users.noreply.github.com>
2021-04-15 11:30:52 +0100

    Update Budget_App.py

--- Budget_App.py
+++ Budget_App.py
@@ -1,12 +1,39 @@
 
 class budget:
-    
-    #balance = {'food':100,'clothing':200}
-    
-    def __init__(self,cat_select): 
+
+    # balance = {'food':100,'clothing':200}
+
+    def __init__(self, cat_select):
         self.cat_select = cat_select
-        
-    database = {'food':100,'clothing':200}
+
+    database = {'food': 100, 'clothing': 200}
+    def balance(self):
+        print(self.categories, 'balance is: ')
+        bul = self.database[self.categories]
+        return categories
+
+    def withdraw(self):
+        withdraw = int(input('Amount to withdraw: \n'))
+        if withdraw <= self.database[self.categories]:
+            print('done')
+            # withdraw_1 = self.categories.balance() - withdraw
+            # return withdraw
+        else:
+            print('Insufficient Amount \n')
+        return withdraw()
+
+    def deposit(self):
+        deposit_amount = int(input('How much did you want to deposit \n'))
+        print('Deposit successful')
+        deposit = self.database['food'] + deposit_amount
+        print(self.database['food'])
+
+    def login():
+        balance()
+
+
+
+def init():
     print('Welcome to Budget App')
     print('Available Options \n')
     print('1 Deposit \n')
@@ -15,44 +42,13 @@ class budget:
     select_action = int(input())
     if select_action == 1:
         cat_select = input('Enter category to fund \n')
-        if cat_select in database:
+        if cat_select in budget.database:
             cat_select = budget(cat_select)
             print('ready')
             cat_select.deposit()
         else:
             print('wrong selection \n')
-        
-               
     else:
         print('Invalid category selected \n')
-    
-    def balance(self):
-        print(self.categories,'balance is: ')
-        bul = database[self.categories]
-        return categories
-        
-    def withdraw(self):
-        withdraw = int(input('Amount to withdraw: \n'))
-        if withdraw <= database[self.categories]:
-            print('done')
-            #withdraw_1 = self.categories.balance() - withdraw
-            #return withdraw
-        else:
-            print('Insufficient Amount \n')
-        return withdraw()
-        
-    def deposit(self):
-        deposit_amount = int(input('How much did you want to deposit \n'))
-        print('Deposit successful')
-        deposit = database['food'] + deposit_amount
-        print(database['food'])
-        
-    def login():
-        balance()
 
-
-
-
-
-   
-    
+init()
